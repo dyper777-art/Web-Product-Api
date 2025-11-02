7faa36f48b26d9e3d61a9d33577bd798ba73c814
ME <me@192.168.1.200>
2025-11-03 03:50:52 +0700

    commit

--- src/main/java/com/setec/controller/MyController.java
+++ src/main/java/com/setec/controller/MyController.java
@@ -10,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +36,15 @@ public class MyController {
 		}
 		
 		return ResponseEntity.status(404).body(Map.of("message", "product is empty."));
+	}
+	
+	@GetMapping("{id}")
+	public Object getById(@PathVariable("id") Integer id) {
+		var product = productRepo.findById(id);
+		if(product.isPresent())
+			return product.get();
+		
+		return ResponseEntity.status(404).body(Map.of("message", "Product id="+id+" not found"));
 	}
 	
 	@PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
