88bc860ea500c0a770617f2334df855698044a90
dyper777-art <dyper777@gmail.com>
2025-11-15 12:48:55 +0700

    Rename entity table from 'tbl_product' to 'tbl_product_Chan_srey_leak'

--- src/main/java/com/setec/entities/Product.java
+++ src/main/java/com/setec/entities/Product.java
@@ -13,7 +13,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity(name = "tbl_product")
+@Entity(name = "tbl_product_Chan_srey_leak")
 @Getter
 @Setter
 @AllArgsConstructor
